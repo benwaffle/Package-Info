ea342c8f174fe4b50b749cd88b0c2e9f6beb952a
Ben Iofel <ben@iofel.me>
2023-05-16 21:22:21 -0400

    search

--- app/src/main/java/me/iofel/packagelist/MainActivity.kt
+++ app/src/main/java/me/iofel/packagelist/MainActivity.kt
@@ -1,21 +1,37 @@
 package me.iofel.packagelist
 
+import android.app.SearchManager
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 
 data class AppInfo(val name: String, val pkg: String, val icon: Drawable)
 
-class AppInfoAdapter(context: Context, private val layoutInflater: LayoutInflater) : ArrayAdapter<AppInfo>(context, R.layout.list_item) {
+class AppInfoAdapter(context: Context, private val layoutInflater: LayoutInflater) : ArrayAdapter<AppInfo>(context, R.layout.list_item), Filterable {
+    val infos = ArrayList<AppInfo>();
+
+    override fun add(info: AppInfo?) {
+        if (info != null) {
+            infos.add(info)
+            super.add(info)
+        }
+    }
+
     override fun getView(pos: Int, v: View?, parent: ViewGroup): View {
         val view = v ?: layoutInflater.inflate(R.layout.list_item, parent, false)
 
@@ -34,17 +50,47 @@ class AppInfoAdapter(context: Context, private val layoutInflater: LayoutInflate
         setNotifyOnChange(true)
         super.notifyDataSetChanged()
     }
+
+    override fun getFilter(): Filter {
+        return MyFilter()
+    }
+
+    inner class MyFilter : Filter() {
+        override fun performFiltering(query: CharSequence): FilterResults {
+            val matches = infos.filter { info -> info.name.contains(query) || info.pkg.contains(query) }
+
+            val results = FilterResults()
+            results.values = matches
+            results.count = matches.size
+            return results
+        }
+
+        override fun publishResults(query: CharSequence, results: FilterResults) {
+            clear()
+            addAll(results.values as List<AppInfo>)
+            notifyDataSetChanged()
+        }
+
+    }
 }
 
 
 class MainActivity : AppCompatActivity() {
+    var adapter: AppInfoAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = AppInfoAdapter(this, this.layoutInflater)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        adapter = AppInfoAdapter(this, this.layoutInflater)
         val lv: ListView = findViewById(R.id.listView)
         lv.adapter = adapter
+
+        if (intent.action == Intent.ACTION_SEARCH) {
+            Toast.makeText(this, intent.getStringExtra(SearchManager.QUERY), Toast.LENGTH_SHORT).show()
+        }
 
         Thread {
             val applications = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
@@ -54,8 +100,29 @@ class MainActivity : AppCompatActivity() {
                     app.packageName,
                     app.loadIcon(packageManager),
                 )
-                runOnUiThread { adapter.add(info) }
+                runOnUiThread { adapter!!.add(info) }
             }
         }.start()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+
+        val sm = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setSearchableInfo(sm.getSearchableInfo(componentName))
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(query: String): Boolean {
+                    adapter!!.filter.filter(query)
+                    return true
+                }
+
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return false
+                }
+            })
+        }
+
+        return true
     }
 }
